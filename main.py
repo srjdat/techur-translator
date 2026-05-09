dc2fd59b33dc26789fb431659957b87ea4d035dc
Srijan Datta <srjdatta@gmail.com>
2026-05-08 23:06:32 -0500

    random

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ sentence = str(input())
 if "techur" in sentence:
     sentence = sentence.replace("techur", "soybook")
     print(sentence)
+else:
+    print("sentence does not have techur in it so will not be translated")
 
 print("hello world")
