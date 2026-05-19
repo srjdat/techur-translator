4a54aa083cd3cabfbb8cc228300cfc207f8c7906
xyve <xyve777@gmail.com>
2026-05-19 00:16:22 -0500

    techur-translator: testing.py loop unless input is exit

--- src/testing.py
+++ src/testing.py
@@ -5,9 +5,11 @@ model = AutoModelForSeq2SeqLM.from_pretrained('tm1')
 tokenizer = AutoTokenizer.from_pretrained('tm1')
 
 #get user input 
-user_input = input("enter your techursentence: ")
+user_input = "" 
 
-#tokenize input and output is what the model generates 
-inputs = tokenizer(user_input, return_tensors='pt').to(model.device)
-outputs = model.generate(**inputs, max_new_tokens=128)
-print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+while user_input != "exit":
+    user_input = input("enter your techursentence: ")
+    #tokenize input and output is what the model generates 
+    inputs = tokenizer(user_input, return_tensors='pt').to(model.device)
+    outputs = model.generate(**inputs, max_new_tokens=128)
+    print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
