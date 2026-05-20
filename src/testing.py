598f110aa071ead72f7168a49ce8bf98b099d768
Srijan Datta <srjdatta7@gmail.com>
2026-05-19 22:00:57 -0500

    made reverse techur translator

--- src/testing.py
+++ src/testing.py
@@ -1,15 +1,34 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-# import local model and tokenizer
-model = AutoModelForSeq2SeqLM.from_pretrained('tm1')
-tokenizer = AutoTokenizer.from_pretrained('tm1')
+# ask user what translation direction they want to go
+model_type = int(input("Which Type of Translation? (Enter Option Number) \n1. Techur to English \n2. English to Techur\n>>> "))
 
-#get user input 
-user_input = "" 
+match model_type:
+    case 1:
+        # import local model and tokenizer
+        model = AutoModelForSeq2SeqLM.from_pretrained('tm1')
+        tokenizer = AutoTokenizer.from_pretrained('tm1')
 
-while user_input != "exit":
-    user_input = input("enter your techursentence: ")
-    #tokenize input and output is what the model generates 
-    inputs = tokenizer(user_input, return_tensors='pt').to(model.device)
-    outputs = model.generate(**inputs, max_new_tokens=128)
-    print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+        # get user input
+        user_input = ""
+        while user_input != "exit":
+            user_input = input("enter your techursentence: ")
+            # tokenize input and output is what the model generates
+            inputs = tokenizer(user_input, return_tensors='pt').to(model.device)
+            outputs = model.generate(**inputs, max_new_tokens=128)
+            print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+
+    case 2:
+        # import local model and tokenizer
+        model = AutoModelForSeq2SeqLM.from_pretrained('rtm1')
+        tokenizer = AutoTokenizer.from_pretrained('rtm1')
+
+        # get user input
+        user_input = ""
+
+        while user_input != "exit":
+            user_input = input("enter your sentence: ")
+            # tokenize input and output is what the model generates
+            inputs = tokenizer(user_input, return_tensors='pt').to(model.device)
+            outputs = model.generate(**inputs, max_new_tokens=128)
+            print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
--- src/train.py
+++ src/train.py
@@ -4,20 +4,23 @@ from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Trainer, TrainingArguments
 import torch
 
+# rtm1 = ReverseTechurModel1 = English to Techur
+# tm1 = TechurModel1 = Techur to English
+
+#tokenize the data
+def tokenize(batch):
+    tokenized_inputs = tokenizer(batch["input"], max_length=128, truncation=True, padding="max_length") #tokenize inputs
+    tokenized_outputs = tokenizer(batch["output"], max_length=128, truncation=True, padding="max_length") #tokenize given outputs
+    tokenized_inputs["labels"] = tokenized_outputs["input_ids"] #labels of inputs should be the outputs
+    return tokenized_inputs
+
 # this is for me since i'm using my cpu to train the model
 torch.set_num_threads(os.cpu_count())
 
 model = AutoModelForSeq2SeqLM.from_pretrained('google/flan-t5-small', device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained('google/flan-t5-small')
 
-#tokenize the data
-def tokenize(batch):
-    tokenized_inputs = tokenizer(batch["input"], max_length=128, truncation=True, padding="max_length") #tokenize inputs
-    tokenized_outputs = tokenizer(batch["output"], max_length=128, truncation=True, padding="max_length") #tokenize given outputs 
-    tokenized_inputs["labels"] = tokenized_outputs["input_ids"] #labels of inputs should be the outputs
-    return tokenized_inputs
-
-
+# This is for TechurModel1, which is Techur to English #
 # tokenize data set 
 dataset = load_dataset('json', data_files='data.json')
 tokenized_dataset = dataset.map(tokenize, batched=True)
@@ -40,3 +43,27 @@ trainer = Trainer(
 trainer.train()
 model.save_pretrained("tm1")
 tokenizer.save_pretrained("tm1")
+
+# This is for ReverseTechurModel1, which is English to Techur #
+# tokenize data set
+dataset = load_dataset('json', data_files='reverse_data.json')
+tokenized_dataset = dataset.map(tokenize, batched=True)
+
+#TrainingArguments
+training_arguments = TrainingArguments(
+    output_dir="training_directory",
+    per_device_train_batch_size=8,
+    num_train_epochs=60,
+    learning_rate=3e-4
+)
+#set up the trainer
+trainer = Trainer(
+    model=model,
+    args=training_arguments,
+    train_dataset=tokenized_dataset["train"]
+)
+
+#train and save the model as tm1 (TechurModel1)
+trainer.train()
+model.save_pretrained("rtm1")
+tokenizer.save_pretrained("rtm1")
